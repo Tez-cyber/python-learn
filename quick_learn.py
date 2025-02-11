35881191653477c5e3ca12b01a815cd9f106d95f
Salami Azeez <salamitopeazeez@gmail.com>
2025-02-11 03:57:30 +0100

    Brief on tuple

--- quick_learn.py
+++ quick_learn.py
@@ -2,3 +2,7 @@
 my_list = [1, 2, "hello", 3.14]
 my_list.append(5)
 print(my_list[0])
+
+# TUPLES ======== Ordered, immutable (unchangeable) sequences
+my_tuple = (1, 2, "hello", 3.14)
+print(my_tuple)
