866ef8cc2f53f1a322df239cec56ed6b2ea19a3b
Salami Azeez <salamitopeazeez@gmail.com>
2025-02-11 03:59:20 +0100

    Brief on dictionary

--- quick_learn.py
+++ quick_learn.py
@@ -6,3 +6,7 @@ print(my_list[0])
 # TUPLES ======== Ordered, immutable (unchangeable) sequences
 my_tuple = (1, 2, "hello", 3.14)
 print(my_tuple)
+
+# DICIONARIES ======== Unordered, mutable (changeable) collection of key-value pairs
+my_dict = {"name": "John", "age": 25}
+print(my_dict["name"])
